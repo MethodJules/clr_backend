a05c487b90216abc6c4660e37d578d688e1ac5ff
MethodJules <julien.hofer@gv.hamburg.de>
2022-07-06 20:48:41 +0200

    Refactored clr recommender endpoint so that ISMLL can use it.

--- docroot/web/modules/custom/clr/src/Plugin/rest/resource/CLRRecommenderLog.php
+++ docroot/web/modules/custom/clr/src/Plugin/rest/resource/CLRRecommenderLog.php
@@ -25,29 +25,32 @@ class CLRRecommenderLog extends ResourceBase {
    */
   public function get() {
 
-    $database = \Drupal::database();
-    $query = $database->query("SELECT * FROM recommender_log");
-    $result = $query->fetchAll();
-
     $rlids = \Drupal::entityQuery('recommender_log')->sort('field_timestamp', 'DESC')->execute();
     $recommenderLogs = RecommenderLog::loadMultiple($rlids);
 
 
     foreach ($recommenderLogs as $recommenderLog) {
+       
         $data[] = [
             'id' => $recommenderLog->id(),
             'timestamp' => $recommenderLog->get('field_timestamp')->value,
             'event' => $recommenderLog->get('field_event')->value,
+            'user_id' => $recommenderLog->get('field_user_id')->value,
             'conceptmap_id' => $recommenderLog->get('field_conceptmap_id')->value,
             'conceptmap_name' => $recommenderLog->get('field_conceptmap_name')->value,
+            'conceptmap_tags' => $recommenderLog->get('field_conceptmap_tags')->value,
             'concepts' => $recommenderLog->get('field_concepts')->value,
-
-        ];
+            'recommender_concept' => $recommenderLog->get('field_recommender_concept')->value,
+        ]; 
     }
 
     $groupedData = $this->groupArray('conceptmap_id', $data);
+
+    foreach ($groupedData as $foo) {
+        $bar[] = $foo[0];
+    }
     
-    $response = new ResourceResponse($groupedData);
+    $response = new ResourceResponse($bar);
     return $response;
   }
 
